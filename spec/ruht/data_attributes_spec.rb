4319d243297c54811e3e73bf10c536a3135e5b75
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-01-28 17:42:39 +0100

    Reify test

--- spec/ruht/data_attributes_spec.rb
+++ spec/ruht/data_attributes_spec.rb
@@ -3,11 +3,11 @@
 require "ruht/data_attributes"
 
 RSpec.describe Ruht::DataAttributes do
-  let(:data_attributes) { described_class.new(behavior: :delete) }
+  let(:data_attributes) { described_class.new(behavior: :delete, key: 3) }
 
   describe "#to_s" do
     it "creates data attributes from hash" do
-      expect(data_attributes.to_s).to eq('data-behavior="delete"')
+      expect(data_attributes.to_s).to eq('data-behavior="delete" data-key="3"')
     end
   end
 end
