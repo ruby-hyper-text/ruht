865b3aac48015b06ac1788c5e7f516353dfba77f
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-01-28 13:22:59 +0100

    Setup rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,18 @@ require "ruht"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = "spec/.rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+    c.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.profile_examples = 3
+  config.order = :random
 end
